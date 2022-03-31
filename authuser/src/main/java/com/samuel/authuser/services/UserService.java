eafa016b9f672ffc23acc95d42ad604c5a21eea8
Samuel <samuelmodestoes@gmail.com>
2022-03-30 22:44:22 -0300

    create UserServices

--- authuser/src/main/java/com/samuel/authuser/services/UserService.java
+++ authuser/src/main/java/com/samuel/authuser/services/UserService.java
@@ -1,4 +1,15 @@
 package com.samuel.authuser.services;
 
+import com.samuel.authuser.models.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
+    List<User> findAll();
+
+    Optional<User> findById(UUID userId);
+
+    void delete(User user);
 }
--- authuser/src/main/java/com/samuel/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/samuel/authuser/services/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.samuel.authuser.services.impl;
 
+import com.samuel.authuser.models.User;
 import com.samuel.authuser.repositories.UserRepository;
 import com.samuel.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,4 +16,18 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
 }
