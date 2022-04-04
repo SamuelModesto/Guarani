6a73aaf04de2312946fd46c8f15ee6d10aa97afa
Samuel <samuelmodestoes@gmail.com>
2022-04-04 20:26:06 -0300

    implemented resolver specification in repository

--- authuser/src/main/java/com/samuel/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/com/samuel/authuser/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package com.samuel.authuser.repositories;
 
 import com.samuel.authuser.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     boolean existsByUsername(String username);
 
