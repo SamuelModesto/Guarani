e0298a9cd9f6f1c32cd3ab3e35b579181749da57
Samuel <samuelmodestoes@gmail.com>
2022-04-18 21:51:04 -0300

    documentation Constraints personalized

--- authuser/src/main/java/com/samuel/authuser/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/samuel/authuser/specifications/SpecificationTemplate.java
@@ -9,6 +9,19 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class SpecificationTemplate {
 
+    /**
+     * Essas specifications servem para receber os argumentos de filtro,
+     * sento path o atributo que se quer filtrar, o Equal serve para filtrar
+     * pelo valor exato de um determinado atribuito, o like serve para filtrar
+     * uma parte, ou seja, se contém uma parte naquele atributo, então
+     * este será filtrado.
+     *
+     * And serve para combinar esses 3 filtros, sendo que existem outros grupos
+     * de specifications que podemos usar.
+     *
+     * O specification deve ser passado no parametro do endpoint.
+     */
+
     @And({
             @Spec(path = "userType", spec = Equal.class),
             @Spec(path = "userStatus", spec = Equal.class),
