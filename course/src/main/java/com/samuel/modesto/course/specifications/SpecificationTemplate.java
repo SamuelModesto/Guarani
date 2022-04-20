799a5e1dbbe65a06fdd5304ae2a93a6c9aeb83d8
Samuel <samuelmodestoes@gmail.com>
2022-04-19 22:49:24 -0300

    implemented specification template

---
+++ course/src/main/java/com/samuel/modesto/course/specifications/SpecificationTemplate.java
@@ -0,0 +1,20 @@
+package com.samuel.modesto.course.specifications;
+
+import com.samuel.modesto.course.models.Course;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    @And({
+            @Spec(path = "courseLevel", spec = Equal.class),
+            @Spec(path = "courseStatus", spec = Equal.class),
+            @Spec(path = "name", spec = Like.class)
+    })
+    public interface CourseSpec extends Specification<Course> {
+
+    }
+}
