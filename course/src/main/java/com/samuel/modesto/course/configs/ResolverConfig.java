bd58e0d5d060b3d01308a3794dd2cc9b0f387588
Samuel <samuelmodestoes@gmail.com>
2022-04-19 22:50:34 -0300

    implemented config for filters

---
+++ course/src/main/java/com/samuel/modesto/course/configs/ResolverConfig.java
@@ -0,0 +1,27 @@
+package com.samuel.modesto.course.configs;
+
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+@Configuration
+public class ResolverConfig extends WebMvcConfigurationSupport {
+
+    /**
+     *
+     * @param argumentResolvers argumentos de entrada no momento da requisição, e ela converte
+     *                          os dados para tipos comuns em java. Após essa configuração basica,
+     *                          basta criar specifications.
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        argumentResolvers.add(resolver);
+        super.addArgumentResolvers(argumentResolvers);
+    }
+}
