31e393ce0189d40430d7b8a77505a816135c1c95
Samuel <samuelmodestoes@gmail.com>
2022-04-19 22:50:04 -0300

    extends interface for pageable

--- course/src/main/java/com/samuel/modesto/course/repositories/CourseRepository.java
+++ course/src/main/java/com/samuel/modesto/course/repositories/CourseRepository.java
@@ -2,8 +2,9 @@ package com.samuel.modesto.course.repositories;
 
 import com.samuel.modesto.course.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface CourseRepository extends JpaRepository<Course, UUID> {
+public interface CourseRepository extends JpaRepository<Course, UUID>, JpaSpecificationExecutor<Course> {
 }
