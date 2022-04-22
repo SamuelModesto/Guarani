cd34d4790a1159046c741e8a32359fc770683269
Samuel <samuelmodestoes@gmail.com>
2022-04-21 22:50:25 -0300

    implemented pageable specificationTemplate

--- course/src/main/java/com/samuel/modesto/course/specifications/SpecificationTemplate.java
+++ course/src/main/java/com/samuel/modesto/course/specifications/SpecificationTemplate.java
@@ -1,11 +1,18 @@
 package com.samuel.modesto.course.specifications;
 
 import com.samuel.modesto.course.models.Course;
+import com.samuel.modesto.course.models.Lesson;
+import com.samuel.modesto.course.models.Module;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -16,5 +23,26 @@ public class SpecificationTemplate {
     })
     public interface CourseSpec extends Specification<Course> {
 
+    }
+
+    @Spec(path = "title", spec = Like.class)
+    public interface ModuleSpec extends Specification<Module> {
+
+    }
+
+    @Spec(path = "title", spec = Like.class)
+    public interface LessonSpec extends Specification<Lesson> {
+
+    }
+
+    public static Specification<Module> searchModulesIntoCourse(final UUID courseId) {
+        return (root, query, criteriaBuilder) -> {
+            query.distinct(true);
+            Root<Module> module = root;
+            Root<Course> course = query.from(Course.class);
+            Expression<Collection<Module>> modulesInCourse = course.get("modules");
+            return criteriaBuilder.and(criteriaBuilder.equal(course.get("courseId"), courseId),
+                    criteriaBuilder.isMember(module, modulesInCourse));
+        };
     }
 }
