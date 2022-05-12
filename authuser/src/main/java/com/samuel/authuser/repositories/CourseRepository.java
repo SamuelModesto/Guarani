6d4ec9a8a4d5c86ede2569311fdbcd08db6ce2d9
Samuel <samuelmodestoes@gmail.com>
2022-05-11 22:58:23 -0300

    implemented course repository

---
+++ authuser/src/main/java/com/samuel/authuser/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.samuel.authuser.repositories;
+
+import com.samuel.authuser.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+}
