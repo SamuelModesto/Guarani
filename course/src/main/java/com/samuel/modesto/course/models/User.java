7921731a03ee6d1476ef44d5dbbbb2bd93bb79a6
Samuel <samuelmodestoes@gmail.com>
2022-05-11 23:00:04 -0300

    created user model

---
+++ course/src/main/java/com/samuel/modesto/course/models/User.java
@@ -0,0 +1,27 @@
+package com.samuel.modesto.course.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "tb_courses_users")
+public class User implements Serializable {
+    final static long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private UUID userId;
+}
