55688aea5c923e0a659a41acb9f7b7349a5360d9
Samuel <samuelmodestoes@gmail.com>
2022-05-20 20:29:53 -0300

    create enums for capture course microservices information

---
+++ authuser/src/main/java/com/samuel/authuser/enums/CourseLevel.java
@@ -0,0 +1,8 @@
+package com.samuel.authuser.enums;
+
+public enum CourseLevel {
+    BEGINNER,
+    INTERMEDIARY,
+    ADVANCED;
+}
+
---
+++ authuser/src/main/java/com/samuel/authuser/enums/CourseStatus.java
@@ -0,0 +1,6 @@
+package com.samuel.authuser.enums;
+
+public enum CourseStatus {
+    INPROGRESS,
+    CONCLUDED;
+}
